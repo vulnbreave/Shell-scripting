f4273318d7ccf83b9789ce8b3a9aa59b9b73708a
vulnbreave8080 <83900370+vulnbreave@users.noreply.github.com>
2024-07-25 20:15:51 +0530

    Add files via upload

---
+++ debugt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pwd
+set -x #Debugging starts from here.  
+uname
+name=whoami
+age=00
+pws
+ps
+id
+echo "My name is $name and my age is $age. Thanks for stopping by."
+set +x #Debugging ends here. 
+lscpu
