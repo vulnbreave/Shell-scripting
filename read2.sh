7066d0773e5df78107395fadc3c1455c13654dcb
vulnbreave8080 <83900370+vulnbreave@users.noreply.github.com>
2024-07-25 20:33:26 +0530

    Add files via upload

---
+++ read2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+name=$1
+age=$2
+echo "Hello, Whats your name?" 
+read name
+echo "Cool! Whats your age?"
+sleep 3 
+read age
+sleep 2
+echo "Is this correct?"
+sleep 5
+echo "Your name is $name"
+sleep 1 
+echo "Your age is $age"
+sleep 1
+echo "See it works!" 
+sleep 2
+echo "You got a eautiful smile there..Keep smiling :)" 
