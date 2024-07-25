16e1f17c5af7b91798ca4692f07938b4eac68895
vulnbreave8080 <83900370+vulnbreave@users.noreply.github.com>
2024-07-25 20:27:22 +0530

    Add files via upload

---
+++ mypasswd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#umm...there's nothing here *smiling in pain* 
+
+passwd=$1
+
+
+echo "HElloOooooooOoooOoOOOO"
+sleep 3
+echo "Guess my password to get the super secret code. (trust me you wont find it hehe :P) : " 
+read passwd
+echo "CHECKINGGGGgggg.......*.*.*.."
+sleep 4 
+if [ $passwd = "BrightSunShine56#@!" ]
+then echo "You found it...Here's the code: 48690"
+else echo "THats wronG! TRYAGAIN...MUHAHAHA"
+fi
+
