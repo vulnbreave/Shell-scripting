81707a2562a32aefa17ea44001e632991c57a814
vulnbreave8080 <83900370+vulnbreave@users.noreply.github.com>
2024-07-25 20:10:56 +0530

    Add files via upload

---
+++ biodata.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+name=$1
+age=$2
+job=$3
+
+echo "Your Name:"
+read name 
+echo "Your Age:"
+read age
+echo "Your Job:"
+read job
+
+echo "To confirm verify the provided given details below."
+
+echo "Name $name"
+echo "Age: $age"
+echo "Job: $job"
+
+sleep 1 
+
+echo "Verifying Request.....%.%.%.%.%.."
+
+sleep 6 
+
+echo "Thankyou Have a nice day!" 
